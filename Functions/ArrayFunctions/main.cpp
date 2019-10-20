bdeaf26772cb1312df94081ca14fa115b1685070
Axel <Axel@201-8>
2019-10-20 10:14:20 +0300

    Sort2D

--- Functions/ArrayFunctions/main.cpp
+++ Functions/ArrayFunctions/main.cpp
@@ -17,6 +17,7 @@ void Print(int Arr[ROWS][COLS], const int ROWS, const int COLS);
 void Sort(int Arr[], const int n);
 void Sort(double Arr[], const int n);
 void Sort(char Arr[], const int n);
+void Sort(int Arr[ROWS][COLS], const int ROWS, const int COLS);
 
 int  Sum(int Arr[], const int n);	//Прототип функции - Function declaration
 double  Sum(double Arr[], const int n);	//Прототип функции - Function declaration
@@ -70,8 +71,8 @@ void main()
 	////////////////////////////////////////////////////////////////
 
 	cout << "Двумерные массивы: " << endl;
-	
-	int Drr[ROWS][COLS] = 
+
+	int Drr[ROWS][COLS] =
 	{
 		{1, 2, 3},
 		{4, 5, 6},
@@ -80,10 +81,13 @@ void main()
 
 	FillRand(Drr, ROWS, COLS);
 	Print(Drr, ROWS, COLS);
-	cout << "Сумма элементов массива: " << Sum(Drr, ROWS, COLS) << endl;
+	cout << endl;
+	/*cout << "Сумма элементов массива: " << Sum(Drr, ROWS, COLS) << endl;
 	cout << "Среднее арифметическое : " << Avg(Drr, ROWS, COLS) << endl;
 	cout << "Минимальное значение в массиве: " << minValueIn(Drr, ROWS, COLS) << endl;
-	cout << "Максимальное значение в массиве: " << maxValueIn(Drr, ROWS, COLS) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(Drr, ROWS, COLS) << endl;*/
+	Sort(Drr, ROWS, COLS);
+	Print(Drr, ROWS, COLS);
 }
 
 void FillRand(int Arr[], const int n)
@@ -216,6 +220,36 @@ void Sort(char Arr[], const int n)
 			}
 		}
 	}
+}
+
+void Sort(int Arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	int iterations = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			for (int k = i; k < ROWS; k++)
+			{
+				int l;
+				if (k == i)l = j + 1; 
+				else l = 0;
+				for (; l < COLS; l++)
+				{
+					//ЕСЛИ перебираемый элемент меньше, чем выбранный,
+					//их нужно поменять местами.
+					if (Arr[k][l] > Arr[i][j])
+					{
+						int buffer = Arr[i][j];
+						Arr[i][j] = Arr[k][l];
+						Arr[k][l] = buffer;
+					}
+					iterations++;
+				}
+			}
+		}
+	}
+	cout << "Массив отсортирован за " << iterations << " итераций." << endl;
 }
 
 int  Sum(int Arr[], const int n)
